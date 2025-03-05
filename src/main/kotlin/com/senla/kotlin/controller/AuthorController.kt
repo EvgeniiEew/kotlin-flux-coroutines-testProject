714a8711ee30a1205913170d8cb8d18e7c4f27a9
EvgeniiEew <evgenii1900@gmail.com>
2025-03-05 14:57:37 +0300

    add mapped diagnostic context

--- src/main/kotlin/com/senla/kotlin/controller/AuthorController.kt
+++ src/main/kotlin/com/senla/kotlin/controller/AuthorController.kt
@@ -3,24 +3,45 @@ package com.senla.kotlin.controller
 import com.senla.kotlin.dto.AuthorDto
 import com.senla.kotlin.service.AuthorService
 import kotlinx.coroutines.flow.Flow
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/author")
 class AuthorController(@Autowired private val authorService: AuthorService) {
+
+    private val logger: Logger = LoggerFactory.getLogger(AuthorController::class.java)
+
     @PostMapping()
     suspend fun saveAuthor(@RequestBody authorDto: AuthorDto): AuthorDto {
-        return authorService.saveAuthor(authorDto)
+        val requestId = UUID.randomUUID().toString()
+        MDC.put("requestId", requestId)
+        try {
+            logger.info("Save new author")
+            return authorService.saveAuthor(authorDto)
+        }finally {
+            MDC.clear()
+        }
     }
 
     @GetMapping("/get")
     suspend fun getAllAuthor(): Flow<AuthorDto> {
-        return authorService.getAllAuthor()
+        val requestId = UUID.randomUUID().toString()
+        MDC.put("requestId", requestId)
+        try {
+            logger.info("Fetching all authors")
+            return authorService.getAllAuthor()
+        } finally {
+            MDC.clear()
+        }
     }
 
 }
