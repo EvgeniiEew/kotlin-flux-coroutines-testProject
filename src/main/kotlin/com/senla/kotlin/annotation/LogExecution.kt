e99829d7d79936f7069596e78c04942afa495dca
EvgeniiEew <evgenii1900@gmail.com>
2025-03-06 16:56:54 +0300

    del aspects , move Logging config

--- src/main/kotlin/com/senla/kotlin/annotation/LogExecution.kt
+++
@@ -1,5 +0,0 @@
-//package com.senla.kotlin.annotation
-//
-//@Target(AnnotationTarget.FUNCTION)
-//@Retention(AnnotationRetention.RUNTIME)
-//annotation class LogExecution
--- src/main/kotlin/com/senla/kotlin/controller/webFilter/LoggingWebFilter.kt
+++ src/main/kotlin/com/senla/kotlin/controller/webFilter/LoggingWebFilter.kt
@@ -1,4 +1,4 @@
-package com.senla.kotlin.config
+package com.senla.kotlin.controller.webFilter
 
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
